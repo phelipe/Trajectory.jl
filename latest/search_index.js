50a2505d5a734b4c0b6ae7e155b3d9905a1a4c2a
autodocs <autodocs>
2018-10-09 22:00:21 +0000

    build based on 7fb47a2

---
+++ latest/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Trajectory",
+    "title": "Trajectory",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#Trajectory-1",
+    "page": "Trajectory",
+    "title": "Trajectory",
+    "category": "section",
+    "text": "Documentation goes here."
+},
+
+]}
